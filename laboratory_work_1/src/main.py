de3479b5916a6714f9f698733ec98515f918a843
pyramixofficial <balamatiuc2@gmail.com>
2024-02-19 15:29:36 +0200

    add more gnerations and remove check

--- laboratory_work_1/src/main.py
+++ laboratory_work_1/src/main.py
@@ -11,12 +11,8 @@ def main():
     S = 'S'
     grammar = Grammar(VN, VT, P, S)
     print("Generated strings:")
-    for _ in range(5):
+    for _ in range(30):
         print(grammar.generate_string())
-    
-    fa = grammar.to_finite_automaton()
-    test_string = "ace" 
-    print(f"Does '{test_string}' belong to the language? {fa.string_belong_to_language(test_string)}")
 
 if __name__ == "__main__":
     main()    
