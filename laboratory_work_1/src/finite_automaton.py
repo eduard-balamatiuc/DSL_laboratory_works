a4bb258ad5942cb3c1e5339255c311b539e393e5
pyramixofficial <balamatiuc2@gmail.com>
2024-02-19 15:18:13 +0200

    remove random prints

--- laboratory_work_1/src/finite_automaton.py
+++ laboratory_work_1/src/finite_automaton.py
@@ -7,8 +7,6 @@ class FiniteAutomaton:
         self.f = f
     
     def string_belong_to_language(self, inputString):
-        print(f"Checking if '{inputString}' belongs to the language...")
-        print(f"self.q: {self.q}, self.sigma: {self.sigma}, self.delta: {self.delta}, self.q0: {self.q0}, self.f: {self.f}")
         currentState = self.q0
         for c in inputString:
             if c not in self.sigma:
