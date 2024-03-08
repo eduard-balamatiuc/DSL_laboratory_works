c82b243c08bf324dc322b2da358504f4d7d96978
pyramixofficial <balamatiuc2@gmail.com>
2024-03-08 11:11:28 +0200

    implement compute_type function and update the structure of the code with separators and improvements in code

--- laboratory_work_2/src/grammar.py
+++ laboratory_work_2/src/grammar.py
@@ -12,12 +12,13 @@ class GrammarTypology(str, Enum):
 
 
 class Grammar:
-    def __init__(self, VN, VT, P, S = "S"):
+    def __init__(self, VN, VT, P, S="S"):
         self.VN = VN
         self.VT = VT
         self.P = P
         self.S = S
-    
+        self.type = self.compute_type()  # Compute the grammar type during initialization
+
     def generate_string(self):
         string = self.S
         while any(v in string for v in self.VN):
@@ -25,7 +26,7 @@ class Grammar:
                 if v in self.VN:
                     string = string.replace(v, random.choice(self.P[v]), 1)
         return string
-    
+
     def to_finite_automaton(self):
         q = self.VN
         sigma = self.VT
@@ -34,14 +35,40 @@ class Grammar:
         for key in self.P.keys():
             delta[key] = [(item[0],) if len(item) == 1 else (item[0], item[1]) for item in self.P[key]]
         values = [item for sublist in self.P.values() for item in sublist]
-        f = [item for item in values if len(set([key for key in self.P.keys()])-set(item)) == len(set([key for key in self.P.keys()]))]
+        f = [item for item in values if len(set(self.P.keys())-set(item)) == len(set(self.P.keys()))]
         return FiniteAutomaton(q, sigma, delta, q0, f)
-    
-    def compute_type(self):
-        is_type_0 = True
-        is_type_1 = False
-        is_type_2 = False
-        is_type_3_right = False
-        is_type_3_left = False
 
-        
+    def compute_type(self):
+        is_type_2 = True
+        is_type_3_right = True
+        is_type_3_left = True
+
+        for left_keys, right_values in self.P.items():
+            if len(left_keys) > 1:
+                return GrammarTypology.RECURSIVELY_ENUMERABLE_GRAMMAR.value
+
+            for right_value in right_values:
+                # Context-sensitive check (any rule where the length of the right_value is less than the left_keys)
+                if len(right_value) < len(left_keys):
+                    return GrammarTypology.RECURSIVELY_ENUMERABLE_GRAMMAR.value
+
+                # Context-free check
+                if any(char in self.VN for char in right_value[1:]):
+                    is_type_2 = False
+
+                # Right-linear check
+                if not (len(right_value) == 1 and right_value[-1] in self.VT) and not (right_value[-1] in self.VN and all(char in self.VT for char in right_value[:-1])):
+                    is_type_3_right = False
+                
+                # Left-linear check
+                if not (len(right_value) == 1 and right_value[0] in self.VT) and not (right_value[0] in self.VN and all(char in self.VT for char in right_value[1:])):
+                    is_type_3_left = False
+
+        if is_type_2:
+            return GrammarTypology.CONTEXT_FREE_GRAMMAR.value
+        if is_type_3_right:
+            return GrammarTypology.RIGHT_LINEAR_GRAMMAR.value
+        if is_type_3_left:
+            return GrammarTypology.LEFT_LINEAR_GRAMMAR.value
+
+        return GrammarTypology.CONTEXT_SENSITIVE_GRAMMAR.value
