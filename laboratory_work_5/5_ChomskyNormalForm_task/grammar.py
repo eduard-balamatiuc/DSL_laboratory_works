96efc1a05d9f4c27050918377c602ba9c68670fa
pyramixofficial <balamatiuc2@gmail.com>
2024-04-22 13:24:25 +0300

    create start symbol working function

--- laboratory_work_5/5_ChomskyNormalForm_task/grammar.py
+++ laboratory_work_5/5_ChomskyNormalForm_task/grammar.py
@@ -1,29 +1,19 @@
 import random
-from enum import Enum
-
+from collections import defaultdict
 
 class Grammar:
-    """Class representing a generalised grammar."""
-    def __init__(self, VN, VT, P, S="S") -> None:
+    """Class representing a generalized grammar."""
+    def __init__(self, VN, VT, P, S="S"):
         self.VN = VN
         self.VT = VT
         self.P = P
         self.S = S
-        self.type = self.compute_type()  # Compute the grammar type during initialization
 
-    def __str__(self) -> str:
+    def __str__(self):
         """Printable representation of the grammar."""
-        return f"{self.type} grammar: VN={self.VN}, VT={self.VT}, P={self.P}, S={self.S}"
+        rules = [f"{v} -> {', '.join(self.P[v])}" for v in self.P]
+        return f"Grammar:\nVN={self.VN}\nVT={self.VT}\nP={rules}\nS={self.S}"
 
-    def generate_string(self) -> str:
-        """Generates a random string from the grammar."""
-        string = self.S
-        while any(v in string for v in self.VN):
-            for v in string:
-                if v in self.VN:
-                    string = string.replace(v, random.choice(self.P[v]), 1)
-        return string
-    
     def to_dict(self) -> dict:
         """Method to export the Grammar to a dictionary format."""
         grammar_dict = {
@@ -33,9 +23,27 @@ class Grammar:
             "S": self.S,
         }
         return grammar_dict
+    
+    def start_symbol_check(self):
+        """Method to check if the start symbol occurs on some right side of the productions.
+        If it does, a new start symbol is created."""
+        if self.S in "".join([element for value_list in self.P.values() for element in value_list]):
+            new_start = f"{self.S}'"
+            self.VN.add(new_start)
+            self.P[new_start] = [self.S]
+            self.S = new_start
 
 
-if __name__=="__main__":
+    def normalize_to_chomsky_normal_form(self):
+        """Method to normalize to Chomsky Normal Form."""
+        self.start_symbol_check()
+        self.remove_null_productions()
+        # self._eliminate_renaming()
+        # self._eliminate_inaccessible_symbols()
+        # self._eliminate_non_productive_symbols()
+        # self._convert_to_chomsky_normal_form()
+
+if __name__ == "__main__":
     # Example usage
     VN = {"S", "A", "B", "C", "D", "E"}
     VT = {"a", "b"}
@@ -43,10 +51,14 @@ if __name__=="__main__":
         "S": ["aB", "AC"],
         "A": ["a", "ASC", "BC", "aD"],
         "B": ["b", "bS"],
-        "C": ["ε", "BA"],
+        "C": ["", "BA"],
         "E": ["aB"],
         "D": ["abC"]
     }
     grammar = Grammar(VN, VT, P)
+    print("Original Grammar:")
+    print(grammar.to_dict())
 
-    
+    grammar.normalize_to_chomsky_normal_form()
+    print("Chomsky Normal Form:")
+    print(grammar.to_dict())
